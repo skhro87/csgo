70187a909e2ae49fe364a47c364f74ef804ea190
Sebastian Krämer <sk.hro87@gmail.com>
2020-06-09 21:54:10 +0800

    add nade practice cfg

--- cp-autoexec.sh
+++ cp-autoexec.sh
@@ -1,3 +1,3 @@
 mv "/Users/seb/Library/ApplicationSupport/Steam/steamapps/common/Counter-Strike Global Offensive/csgo/cfg/autoexec.cfg" "/Users/seb/Library/ApplicationSupport/Steam/steamapps/common/Counter-Strike Global Offensive/csgo/cfg/autoexec.cfg.backup"
 cp autoexec.cfg "/Users/seb/Library/ApplicationSupport/Steam/steamapps/common/Counter-Strike Global Offensive/csgo/cfg/autoexec.cfg"
-
+cp practice.cfg "/Users/seb/Library/ApplicationSupport/Steam/steamapps/common/Counter-Strike Global Offensive/csgo/cfg/practice.cfg"
